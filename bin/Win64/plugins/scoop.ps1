c2736fa2f996a4611642f2a8814f3549c79cc73f
dbyoung720 <dbyoung@sina.com>
2020-04-09 22:17:04 +0800

    add scoop.ps1

---
+++ bin/Win64/plugins/scoop.ps1
@@ -0,0 +1,8 @@
+$env:SCOOP='E:\dbyoung\PBox\bin\Win64\plugins\scoop'
+[environment]::setEnvironmentVariable('SCOOP',$env:SCOOP,'User')
+iwr -useb get.scoop.sh | iex
+
+scoop install aria2
+scoop config aria2-max-connection-per-server 16
+scoop config aria2-split 16
+scoop config aria2-min-split-size 1M
