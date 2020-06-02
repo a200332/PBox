2469ed77a20eae4ab101f741efdebade377af72c
dbyoung720 <dbyoung@sina.com>
2020-06-02 22:02:27 +0800

    modify NP2.cpp

--- DOC/VC/Dialog/Notepad2/NP2.cpp
+++ DOC/VC/Dialog/Notepad2/NP2.cpp
@@ -2,7 +2,7 @@
 
 HINSTANCE hinst = NULL;
 
-BOOL APIENTRY DllMain( HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
 {
     switch (ul_reason_for_call)
     {
