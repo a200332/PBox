b5f7e779468e38af78be5143d9f704dbb64b28fe
dbyoung720 <dbyoung@sina.com>
2020-06-04 14:12:54 +0800

    add 7z-zip vc dlg dll module

---
+++ DOC/VC/Dialog/7zFM/7zFM.cpp
@@ -0,0 +1,37 @@
+﻿#include <windows.h>
+
+HINSTANCE hinst = NULL;
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+    switch (ul_reason_for_call)
+    {
+      case DLL_PROCESS_ATTACH:
+        hinst = (HINSTANCE)hModule;
+      case DLL_THREAD_ATTACH:
+      case DLL_THREAD_DETACH:
+      case DLL_PROCESS_DETACH:
+          break;
+    }
+    return TRUE;
+}
+
+enum TVCDllType {vtDialog, vtMFC};
+
+extern int WINAPI WinMain(HINSTANCE hInstance,HINSTANCE hPrevInst,LPSTR lpCmdLine,int nCmdShow);
+
+extern "C" __declspec(dllexport) void db_ShowDllForm_Plugins(TVCDllType* spFileType, char** strParentName, char** strSubModuleName, char** strIconFileName, char** strClassName, char** strWindowName, const bool show = false)
+{
+    * spFileType       = vtDialog;        // TVCDllType
+    * strParentName    = "文本编辑";      // 父模块名称
+    * strSubModuleName = "7-Zip(DLL)";    // 子模块名称
+    * strClassName     = "FM";            // 窗体类名
+    * strWindowName    = "7-zip";         // 窗体名
+    * strIconFileName  = "";
+    
+    if (show) 
+    {
+      WinMain(hinst, 0, (LPSTR)"", (int)show);
+    }
+}
+
---
+++ DOC/VC/Dialog/7zFM/x64.cmd
@@ -0,0 +1,10 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+
+set "include=C:\Program Files (x86)\Windows Kits\10\Include\10.0.17763.0\ucrt;C:\Program Files (x86)\Windows Kits\10\Include\10.0.17763.0\shared;C:\Program Files (x86)\Windows Kits\10\Include\10.0.17763.0\um;%include%"
+set "lib=C:\Program Files (x86)\Windows Kits\10\Lib\10.0.17763.0\ucrt\x64;C:\Program Files (x86)\Windows Kits\10\Lib\10.0.17763.0\um\x64;%lib%"
+
+cl /c  /Zi /nologo /W3 /WX- /diagnostics:classic /O2 /Oy- /GL /D x64 /D STATIC_BUILD /D SCI_LEXER /D BOOKMARK_EDITION /D NDEBUG /D _CRT_SECURE_NO_WARNINGS /D _UNICODE /D UNICODE /Gm- /EHsc /MT /GS /arch:SSE2 /fp:precise /Zc:wchar_t /Zc:forScope /Zc:inline /Gd /analyze- /FC  7zFM.cpp
+
+link /dll -out:7z-zip.dll /DELAYLOAD:mpr.dll -nologo -RELEASE -OPT:REF -OPT:ICF -LTCG /LARGEADDRESSAWARE /FIXED:NO 7zFM.obj x64\*.obj x64\Format7z\*.obj x64\resource.res comctl32.lib htmlhelp.lib comdlg32.lib Mpr.lib Gdi32.lib delayimp.lib oleaut32.lib ole32.lib user32.lib advapi32.lib shell32.lib
+
+pause
