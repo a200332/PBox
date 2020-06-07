aa56fbf29e8a45c0daede97f00b13106d89b6f88
dbyoung720 <dbyoung@sina.com>
2020-06-08 07:39:47 +0800

    modify 7zzip x64.cmd

---
+++ DOC/VC/Dialog/7zFM/readme.txt
@@ -0,0 +1,11 @@
+﻿7-zip 源代码不做任何修改，进行编译，将生成的 OBJ，RES，库文件复制过来就可以了。
+7-zip source code is compiled without any modification, and the generated OBJ, RES and Library files can be copied.
+
+https://github.com/XhmikosR/7-Zip.git
+
+参考(Reference)
+  将 VC 窗体 EXE 转换成 DLL，供其它语言调用
+  Convert VC form EXE to DLL for other languages to call
+
+  https://blog.csdn.net/dbyoung/article/details/103987103
+  
--- DOC/VC/Dialog/7zFM/x64.cmd
+++ DOC/VC/Dialog/7zFM/x64.cmd
@@ -5,6 +5,6 @@ set "lib=C:\Program Files (x86)\Windows Kits\10\Lib\10.0.17763.0\ucrt\x64;C:\Pro
 
 cl /c  /Zi /nologo /W3 /WX- /diagnostics:classic /O2 /Oy- /GL /D x64 /D STATIC_BUILD /D SCI_LEXER /D BOOKMARK_EDITION /D NDEBUG /D _CRT_SECURE_NO_WARNINGS /D _UNICODE /D UNICODE /Gm- /EHsc /MT /GS /arch:SSE2 /fp:precise /Zc:wchar_t /Zc:forScope /Zc:inline /Gd /analyze- /FC  7zFM.cpp
 
-link /dll -out:7z-zip.dll /DELAYLOAD:mpr.dll -nologo -RELEASE -OPT:REF -OPT:ICF -LTCG /LARGEADDRESSAWARE /FIXED:NO 7zFM.obj x64\*.obj x64\Format7z\*.obj x64\resource.res comctl32.lib htmlhelp.lib comdlg32.lib Mpr.lib Gdi32.lib delayimp.lib oleaut32.lib ole32.lib user32.lib advapi32.lib shell32.lib
+link /dll -out:7zFM.dll /DELAYLOAD:mpr.dll -nologo -RELEASE -OPT:REF -OPT:ICF -LTCG /LARGEADDRESSAWARE /FIXED:NO 7zFM.obj x64\*.obj x64\Format7z\*.obj x64\resource.res comctl32.lib htmlhelp.lib comdlg32.lib Mpr.lib Gdi32.lib delayimp.lib oleaut32.lib ole32.lib user32.lib advapi32.lib shell32.lib
 
 pause
